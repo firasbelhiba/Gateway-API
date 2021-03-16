3e67039ccc534796d70aba01955fe9eef08f8e71
firasbelhiba <firas.belhiba@esprit.tn>
2021-03-16 12:01:00 +0100

    Database Connected ! (MDB)

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+//Connect Database 
+connectDB();
 
 const app = express();
 
@@ -8,4 +12,4 @@ app.get('/', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
