69cc6272725715ed072168fbebaccc4f592ef903
firasbelhiba <firas.belhiba@esprit.tn>
2021-03-17 11:16:07 +0100

    Auth middleware

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ app.get('/', async (req, res) => {
 //Init middleware (Body Parser , now it s included with express )
 app.use(express.json({ extended: false }));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
