7d52b658d397ada2ae57e1dbdfb157e5a21622e3
ghadakhedri1 <61046136+ghadakhedri1@users.noreply.github.com>
2021-03-21 12:25:40 +0100

    unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -120,4 +120,37 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+//@route PUT api/posts/unlike/:id
+//@desc Unlike a post
+//@access Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post is already liked by the user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ message: "Post not liked !" });
+    }
+
+    //Remove Index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ message: "Post not Found " });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
