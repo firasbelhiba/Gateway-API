79fa50be27721bf6a34e54bc89d43678360fde7f
ghadakhedri1 <61046136+ghadakhedri1@users.noreply.github.com>
2021-03-21 12:08:26 +0100

    like post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -94,4 +94,30 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+//@route PUT api/posts/like/:id
+//@desc Like a post
+//@access Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post is already liked by the user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ message: "Post already liked !" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ message: "Post not Found " });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
