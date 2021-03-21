ff216717286328d2bd3099a4497ef1d15077999d
firasbelhiba <firas.belhiba@esprit.tn>
2021-03-21 18:16:55 +0100

    Scrape github repos from user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
@@ -593,6 +595,39 @@ router.post("/report/:id", auth, async (req, res) => {
             return res.status(404).json({ message: "Post not Found " });
         }
 
+        res.status(500).send("Server error");
+    }
+});
+
+
+//@author Firas Belhiba
+//@route GET api/profile/github/:username
+//@desc Get user repos from Github 
+//@access Public 
+router.get('/github/:username', async (req, res) => {
+    try {
+        // This is only to show  repositories , if you want to show more or less just change the per_page parameters in the uri  
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+
+                return res.status(404).json({ msg: 'No Github profile found' });
+            }
+
+            res.json(JSON.parse(body));
+        })
+
+
+
+    } catch (error) {
+        console.error(error.message);
         res.status(500).send("Server error");
     }
 });
