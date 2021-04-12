3b2d4d7cdfb50657b58a306d609cbdd25df33cf4
firasbelhiba <firas.belhiba@esprit.tn>
2021-04-12 18:42:15 +0100

    Login output modification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -65,7 +65,7 @@ router.post('/', [
 
         jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
             if (err) throw err;
-            res.json({ token });
+            res.json({ token, user });
         });
 
 
