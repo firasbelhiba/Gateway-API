c598a9b2b82a7f9f30147d89637e0f08ed6cec34
ghadakhedri1 <61046136+ghadakhedri1@users.noreply.github.com>
2021-04-17 21:46:15 +0100

    multer rectif

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,46 +1,36 @@
-const multer = require('multer');
-const express = require('express');
-const path = require('path');
-const cloudinary = require('../utils/cloudinary');
+const multer = require("multer");
+const express = require("express");
+const path = require("path");
+const cloudinary = require("../utils/cloudinary");
 
-
-//specify the storage engine 
+//specify the storage engine
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${file.filename}-${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      `${file.filename}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
 function checkFileType(file, cb) {
-    const filetypes = /jpg|jpeg|png/
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-    const mimetype = filetypes.test(file.mimetype)
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-    if (extname && mimetype) {
-        return cb(null, true)
-    } else {
-        cb('Image Only!')
-    }
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Image Only!");
+  }
 }
 
-
 const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb)
-    }
-})
-
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
 
 module.exports = upload;
-
-
-
-
-
-
