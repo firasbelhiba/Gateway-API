e4c6ddb929fd758ddb139bf7c16e817325e36ce4
ghadakhedri1 <61046136+ghadakhedri1@users.noreply.github.com>
2021-04-25 01:14:51 +0100

    stackoverflow rectif

--- routes/api/scrape.js
+++ routes/api/scrape.js
@@ -6,10 +6,12 @@ const fs = require("fs");
 const jobTitle = fs.readFileSync("././data/stackoverflow/jobTitles.json");
 const aboutJob = fs.readFileSync("././data/stackoverflow/aboutJob.json");
 const jobImage = fs.readFileSync("././data/stackoverflow/jobImage.json");
+const jobLinks = fs.readFileSync("././data/stackoverflow/SOLinks.json");
 
 let listOfJobsST = JSON.parse(jobTitle);
 let listOfaboutJobST = JSON.parse(aboutJob);
 let listOfjobImageST = JSON.parse(jobImage);
+let listOfLinksST = JSON.parse(jobLinks);
 
 //To change the page of the jobs use query params ?pg=n (n is the number of the page you want to scrape)
 
@@ -18,7 +20,13 @@ let listOfjobImageST = JSON.parse(jobImage);
 //@desc scrape jobs
 //@access Private
 router.get("scrape-stackoverflow", auth, async (req, res) => {
-  scrapeJobStackoverflow("https://stackoverflow.com/jobs?pg=2");
+  try {
+    scrapeJobStackoverflow("https://stackoverflow.com/jobs?pg=2");
+    res.json("scraped with success ! ");
+  } catch (e) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
 //@author Ghada Khedri
@@ -27,6 +35,8 @@ router.get("scrape-stackoverflow", auth, async (req, res) => {
 //@access Private
 router.get("/get-scraped-data-stackoverflow", auth, async (req, res) => {
   try {
+    //scrapeJobStackoverflow("https://stackoverflow.com/jobs?pg=2");
+
     let scrapeList = [];
     let scrapeType = [];
     let scrapeExperience = [];
@@ -43,6 +53,7 @@ router.get("/get-scraped-data-stackoverflow", auth, async (req, res) => {
       scrapeList.push({
         title: listOfJobsST[i],
         image: listOfjobImageST[i],
+        link: listOfLinksST[i],
         type: scrapeType[i],
         experience: scrapeExperience[i],
       });
